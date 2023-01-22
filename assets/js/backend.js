c2682df74b1b1187d460687f960908182d85cc49
leemon <tacrapo@gmail.com>
2023-01-22 10:28:27 +0100

    Migrate jQuery code to vanilla JS

--- assets/js/backend.js
+++ assets/js/backend.js
@@ -1,67 +1,80 @@
 /*
  * calls the wordpress media manager and saves image url to input field
  */
-jQuery( function( $ ) {
+(function () {
 
-    // Instantiates the variable that holds the media library frame.
-    var background_image_frame;
+	document.addEventListener('DOMContentLoaded', function (e) {
 
-    /**
-     * Opens the wordpress media manager frame and sets actions for 
-     * when the user makes their selections
-     */
-    $( '#fbi-set-image, #fbi-update-image' ).on( 'click', function( e ) {
- 
-        // Prevents the default action from occuring.
-        e.preventDefault();
+		// Instantiates the variable that holds the media library frame.
+		var background_image_frame;
 
-        // If the frame already exists, re-open it.
-        if ( background_image_frame ) {
-            background_image_frame.open();
-            return;
-        }
+		/**
+		 * Opens the wordpress media manager frame and sets actions for 
+		 * when the user makes their selections
+		 */
+		document.querySelectorAll('#fbi-set-image, #fbi-update-image').forEach(function (item) {
+			item.addEventListener('click', function (e) {
+				e.preventDefault();
 
-        // Sets up the media library frame
-        background_image_frame = wp.media.frames.background_image_frame = wp.media({
-            title: meta_image.title,
-            button: { text: meta_image.button },
-            library: { type: 'image' }
-        } );
+				// If the frame already exists, re-open it.
+				if (background_image_frame) {
+					background_image_frame.open();
+					return;
+				}
 
-        // Runs when an image is selected.
-        background_image_frame.on( 'select', function() {
-            
-            // Grabs the attachment selection and creates a JSON representation of the model.
-            var media_attachment = background_image_frame.state().get( 'selection' ).first().toJSON();
+				// Sets up the media library frame.
+				background_image_frame = wp.media.frames.background_image_frame = wp.media({
+					title: meta_image.title,
+					button: { text: meta_image.button },
+					library: { type: 'image' }
+				});
 
-            // displays a thumbnail of the selected image
-            $( '#fbi-thumbnail' ).removeClass( 'hide' );
-			$( '#fbi-thumbnail img' ).attr( 'src', media_attachment.url );
+				// Runs when an image is selected.
+				background_image_frame.on('select', function () {
 
-            // Sends the attachment URL to our custom image input field.
-            $( '#fbi-image' ).val( media_attachment.url );
+					// Grabs the attachment selection and creates a JSON representation of the model.
+					var media_attachment = background_image_frame.state().get('selection').first().toJSON();
 
-            // hide / show appropriate links
-            $( '#fbi-set-image' ).addClass( 'hide' );
-            $( '#fbi-image-desc' ).removeClass( 'hide' );
-            $( '#fbi-remove-image' ).removeClass( 'hide' );
-        } );
- 
-        // Opens the media library frame.
-        background_image_frame.open();
-    } );
+					// Displays a thumbnail of the selected image.
+					document.querySelector('#fbi-thumbnail').classList.remove('hide');
+					document.querySelector('#fbi-thumbnail img').setAttribute('src', media_attachment.url);
 
-	/**
-	* removes thumbnail and displays "add new" link
-	*/
-	$( '#fbi-remove-image' ).on( 'click', function( e ) {
-		e.preventDefault();
-		$( '#fbi-thumbnail' ).addClass( 'hide' );
-		$( '#fbi-thumbnail img' ).attr( 'src', '' );
-		$( '#fbi-image' ).val( '' );
-		$( '#fbi-set-image' ).removeClass( 'hide' );
-		$( '#fbi-image-desc' ).addClass( 'hide' );
-		$( '#fbi-remove-image' ).addClass( 'hide' );
-	} );
+					// Sends the attachment URL to our custom image input field.
+					document.querySelector('#fbi-image').value = media_attachment.url;
 
-} );
+					// Hide/Show appropriate links.
+					document.querySelector('#fbi-set-image').classList.add('hide');
+					document.querySelector('#fbi-image-desc').classList.remove('hide');
+					document.querySelector('#fbi-remove-image').classList.remove('hide');
+				});
+
+				// Opens the media library frame.
+				background_image_frame.open();
+			});
+		});
+
+		/**
+		 * Removes thumbnail and displays "Add new" link
+		 */
+		var elRemove = document.querySelector('#fbi-remove-image');
+		if (elRemove) {
+			elRemove.addEventListener('click', function (e) {
+				e.preventDefault();
+
+				// Hides the thumbnail of the selected image.
+				document.querySelector('#fbi-thumbnail').classList.add('hide');
+				document.querySelector('#fbi-thumbnail img').setAttribute('src', '');
+
+				// Remove the attachment URL in our custom image input field.
+				document.querySelector('#fbi-image').value = '';
+
+				// Hide/Show appropriate links.
+				document.querySelector('#fbi-set-image').classList.remove('hide');
+				document.querySelector('#fbi-image-desc').classList.add('hide');
+				document.querySelector('#fbi-remove-image').classList.add('hide');
+			});
+		}
+
+	});
+
+})();
